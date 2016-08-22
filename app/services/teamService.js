3ddc66dde3c27e30b5e8f7f33bfd10aec7f7dcf1
nextitsoftsys <yadav.jayprakash20@yahoo.in>
2016-08-22 18:46:06 +0530

    angulajs added

--- app/services/teamService.js
+++ app/services/teamService.js
@@ -8,7 +8,7 @@ angular.module('bridgeApp')
 
                 //json data
                 var team = ' { "Team": [' + '{"image":"images/face-two.jpg","name":"Narayan Mahadevan", "ocupation":"CTO Mauj Mobile Advisor & Technologist","email":"https://in.linkedin.com/in/narayanprofile"},' +
-                                            '{"image":"images/ashish.jpg","name": "Ashes Vats","ocupation":"Engineering Head and Founding Member","email":"https://in.linkedin.com/in/narayanprofile"},' +
+                                            '{"image":"images/ashish.jpg","name": "Ashes Vats","ocupation":"Engineering Head and Founding Member","email":"        https://in.linkedin.com/in/ashes-vats-50734046"},' +
                                             '{"image": "images/vidya.jpg","name": " Shreevidya Ramamurthy ","ocupation":"Relationship Head and Founding Member","email":"https://in.linkedin.com/in/narayanprofile"}]}';
 
     //converting json object to string object                                          
