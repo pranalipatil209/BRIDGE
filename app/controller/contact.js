d867db01db49cae39249b2ecde6956e678542801
nextitsoftsys <yadav.jayprakash20@yahoo.in>
2016-09-14 09:25:04 +0530

    updated

--- app/controller/contact.js
+++ app/controller/contact.js
@@ -13,13 +13,13 @@ $scope.codeStatus = "";
         // Accessing the Angular $http Service to send data via REST Communication to Node Server.
      $http.post('https://bridge941.herokuapp.com/setData',formData).success(function (response) {
                 console.log("success"); // Getting Success Response in Callback
-                $scope.codeStatus = response.data;
-                console.log(response);
+              
+                
             }).
             error(function (response) {
                 console.log("error"); // Getting Error Response in Callback
                 $scope.codeStatus = response || "Request failed";
-                console.log($scope.codeStatus);
+               
             });
         // Calling the list function in Angular Controller to show all current data in HTML
         return false;
