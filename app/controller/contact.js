6a1a8657b809a11f2e05bfef0db6aa66359d3421
nextitsoftsys <yadav.jayprakash20@yahoo.in>
2016-09-14 14:47:56 +0530

    updated

--- app/controller/contact.js
+++ app/controller/contact.js
@@ -13,6 +13,7 @@ angular.module('bridgeApp').controller('contactCtrl', function ($scope, $http, $
                 // Accessing the Angular $http Service to send data via REST Communication to Node Server.
                 $http.post('https://bridge941.herokuapp.com/setData', formData).success(function (response) {
                     console.log("success"); // Getting Success Response in Callback
+                    alert(response);
                     $scope.codeStatus = response.data;
                         console.log(response)
                 }).error(function (response) {
