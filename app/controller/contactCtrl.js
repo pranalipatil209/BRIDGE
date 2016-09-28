487958b38bd63ff70e95d4ea40955b78c181b8d7
nextitsoftsys <yadav.jayprakash20@yahoo.in>
2016-09-28 14:54:40 +0530

    message function added

--- app/controller/contactCtrl.js
+++ app/controller/contactCtrl.js
@@ -3,12 +3,12 @@ angular.module('bridgeApp').controller('contactCtrl', function ($scope, $http, $
     $scope.codeStatus = "";
     $scope.submit = function () {
               var formData = {
-                    'name': this.name,
-                    'email': this.email,
-                    'subject': this.subject,
-                    'profile': this.profile,
-                    'message': this.message
-                }
+                            'name': this.name,
+                            'email': this.email,
+                            'subject': this.subject,
+                            'profile': this.profile,
+                            'message': this.message
+                            }
               
                 // Accessing the Angular $http Service to send data via REST Communication to Node Server.
                 $http.post('https://bridge941.herokuapp.com/setData', formData).success(function (response) {
